55ad27404293f0ea0557889464fb35ac9d38ca04
Shubhamrazz <sr99845@gmail.com>
2022-04-30 18:29:13 +0530

    Logging Addition program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Addition obj = new Addition();
+            Console.WriteLine("Result:- " + obj.Sum(1, 4));
+        }
+    }
+}
